23ec44786a36448dcbb61c3d94d7fe770c98be69
Nano <nano@srlabs.id>
2021-12-09 13:56:32 +0700

    Fix can't load antd style in publishing

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,4 @@
+import 'antd/dist/antd.css'
 import '../src/stories/styles/global.css'
 
 export const parameters = {
--- src/stories/index.js
+++ src/stories/index.js
@@ -1,3 +1,4 @@
+import 'antd/dist/antd.css'
 import './styles/global.css'
 
 import Alert from './components/Alert'
